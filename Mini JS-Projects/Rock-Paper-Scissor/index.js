a638f895ce6b0388b18e1edb2d530d9f7f40cbe6
saurbh264 <mastersaurabh264@gmail.com>
2024-02-21 10:53:40 +0530

    Frontend completed

---
+++ Mini JS-Projects/Rock-Paper-Scissor/index.js
@@ -0,0 +1,7 @@
+const rock=document.querySelector(".option1");
+const paper=document.querySelector(".option2");
+const scissors=document.querySelector(".option3");
+const resulttext=document.querySelector(".result-text");
+var computer=document.querySelector(".comp-img");
+var user=document.querySelector(".user-img");
+
